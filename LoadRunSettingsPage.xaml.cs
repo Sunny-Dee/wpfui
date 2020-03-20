35b025f1f28177793652803b1b74240c5f3ba48d
deescoba <deescoba@microsoft.com>
2020-03-20 14:07:46 -0700

    Initial template

---
+++ LoadRunSettingsPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dart
+{
+    /// <summary>
+    /// Interaction logic for LoadRunSettingsPage.xaml
+    /// </summary>
+    public partial class LoadRunSettingsPage : Page
+    {
+        public LoadRunSettingsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFromBuild(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadFromFile(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoadFromArtifact(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ StartPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dart
+{
+    /// <summary>
+    /// Interaction logic for StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStart(object sender, RoutedEventArgs e)
+        {
+            var loadPage = new LoadRunSettingsPage();
+            NavigationService.Navigate(loadPage);
+        }
+    }
+}
